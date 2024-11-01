f16c37b400ed0122ff8416c06d0f8b4578032139
robert <robert@arles.us>
2024-11-01 21:24:49 +0000

    just saving unk changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { AtpAgent, AtpSessionEvent, AtpSessionData } from '@atproto/api';
-import { inspect } from 'util';
 
 async function main() {
   // configure connection to the server, without account authentication
-  const agent = new AtpAgent({
-    service: 'https://bsky.com',
-    persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
-      console.log(inspect(sess, { colors: true }))   // store the session-data for reuse
-    },
-  })
+  const agent = new AtpAgent(
+    {
+      service: 'https://bsky.social',
+      persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
+        console.log(require('util').inspect(sess, { colors: true }));   // store the session-data for reuse
+        console.log(require('util').inspect(evt, { colors: true })); // display evt)
+      },
+    });
 
 
   // 2) if an existing session was securely stored previously, then reuse that to resume the session.
@@ -16,8 +17,8 @@ async function main() {
 
   // 3) if no old session was available, create a new one by logging in with password (App Password)
   await agent.login({
-    identifier: process.env.ATRA_USERNAME,
-    password: process.env.ATRA_PASSWORD
+    identifier: process.env.ATRA_USERNAME || "",
+    password: process.env.ATRA_PASSWORD || ""
   })
 
   console.log(agent.did)
